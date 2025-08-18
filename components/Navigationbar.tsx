e2d03f9b54d022df7dd0428a2a0f66dc5c65231b
Paul Tidings Steiner <adjeisteiner@gmail.com>
2025-08-18 16:53:30 +0000

    New button and design added to the navigation bar for web view

--- components/Navigationbar.tsx
+++ components/Navigationbar.tsx
@@ -8,13 +8,16 @@ export default function NavigationBar() {
   const navigation = useNavigation();
   return (
     <View className="flex-row justify-between items-center px-4 py-3 bg-zinc-900 w-full">
-      <TouchableOpacity>
+      <TouchableOpacity className={`p-3 mb-2 rounded-md bg-zinc-800`}>
         <FontAwesomeIcon name="paper-plane" size={16} color="white" />
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity className={`p-3 mb-2 rounded-md`}>
+        <FontAwesomeIcon name="shopping-cart" size={16} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity className={`p-3 mb-2 rounded-md`}>
         <FontAwesomeIcon name="suitcase" size={16} color="white" />
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity className={`p-3 mb-2 rounded-md`}>
         <Icon name="settings" size={16} color="white" />
       </TouchableOpacity>
     </View>
